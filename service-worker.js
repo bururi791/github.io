5b3558071b75db76c77f3a8962485f7b71c341b9
bururi791 <makuskinmatvej170@gmail.com>
2025-07-17 15:45:16 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'singapore-v2'; // Измените версию
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'index.html',
+  'css/style.css',
+  'images/icon-192x192.png',
+  'images/icon-512x512.png',
+  'images/smart-transport.jpg',
+  'images/garden-bridge.jpg',
+  'images/digital-services.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+      .catch(err => {
+        console.error('Cache addAll failed:', err);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
